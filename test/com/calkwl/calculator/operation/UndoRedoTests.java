defff70910fd513d6c605ddd6ad400e26585991d
Caroline Leung <calkwl@hotmail.com>
2012-04-27 18:17:28 -0700

    CL: Added tests for undo/redo operation

---
+++ test/com/calkwl/calculator/operation/UndoRedoTests.java
@@ -0,0 +1,24 @@
+package com.calkwl.calculator.operation;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.calkwl.calculator.IUndoRedoCalculator;
+import com.calkwl.calculator.UndoRedoCalculator;
+
+public class UndoRedoTests {
+
+	@Test
+	public void testUndoRedo() {
+		IUndoRedoCalculator calculator = new UndoRedoCalculator();
+		calculator.apply(new AddOperation(0, 10));
+		Assert.assertEquals(10.0, calculator.getCurrValue());
+		calculator.undoLastOperation();
+		Assert.assertEquals(0.0, calculator.getCurrValue());		
+		calculator.redoLastOperation();
+		Assert.assertEquals(10.0, calculator.getCurrValue());
+
+	}
+
+}
